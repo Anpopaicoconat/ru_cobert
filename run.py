7c1bc165730cbb3a6dab2566008b51e042f56dfa
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2022-02-10 14:06:27 +0300

    Update run.py

--- run.py
+++ run.py
@@ -49,7 +49,7 @@ for proc_data, bert_path in zip(proc_data_path_list, bert_path_list):
                 log_path = 'logs/' + bert_path.split('/')[-1] + '_' + proc_data.split('/')[-1].split('.')[0] + '_interaction' + str(apply_interaction) \
                 + '_' + aggregation_method + '_' + padding_side + '.csv'
                 print(tuple(os.walk('logs/')))
-                if log_path in tuple(os.walk('logs/'))[1]:
+                if log_path in tuple(os.walk('logs/'))[0][1]:
                     print('skeep', log_path)
                     continue
 
