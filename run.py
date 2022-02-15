bb6702892121c5715c000ad9d2773b25e591b9a2
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2022-02-15 10:31:16 +0300

    Update run.py

--- run.py
+++ run.py
@@ -45,9 +45,9 @@ bert_path_list = ["models/enbert", "models/rubert"]
 for proc_data, bert_path in zip(proc_data_path_list, bert_path_list):
     if proc_data != "data/personachat/enpersonachat.txt":
         continue
-    for apply_interaction in range(1, 2):
-        for aggregation_method in ['max', 'mean', 'cls', 'meanmax']: #'max', 'mean', 'meanmax', 'cls' 
-            for padding_side in ['left', 'right']: #'left', 'right'
+    for apply_interaction in range(1):
+        for aggregation_method in ['cls']: #'max', 'mean', 'meanmax', 'cls' 
+            for padding_side in ['right']: #'left', 'right'
                 log_path = bert_path.split('/')[-1] + '_' + proc_data.split('/')[-1].split('.')[0] + '_interaction' + str(apply_interaction) \
                 + '_' + aggregation_method + '_' + padding_side + '.csv'
                 if log_path in tuple(os.walk('logs/'))[0][2]:
