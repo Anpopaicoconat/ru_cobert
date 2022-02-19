c466f0e13f07c8c83a41ce550067984db989bb7c
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2022-02-19 22:29:45 +0300

    Update run_gpt.py

--- run_gpt.py
+++ run_gpt.py
@@ -13,17 +13,19 @@ from util import logger
 with open('config.json', 'r') as config:
     config  = json.loads(config.read())
 
+model_path = config['model_path']
+padding_side = config['padding_side']
 save_model_path = config['save_model_path']
-model_path = config['bert_path']
+
 gradient_accumulation_steps = config['gradient_accumulation_steps'] 
-
+apply_interaction = config['apply_interaction'] 
 matching_method = config['matching_method']
-
+aggregation_method = config['aggregation_method']
 lr =  config['lr'] 
 warmup_steps = config['warmup_steps']
 test_mode = config['test_mode']
 has_persona = config['has_persona']
-padding_side = 'right'
+log_path = config['log_path']
 
 context_len = config['context_len']
 responce_len = config['responce_len']
@@ -40,54 +42,50 @@ schedulers = []
 weight_decay = 0
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-log_path = model_path.split('/')[-1] + '_' + proc_data.split('/')[-1].split('.')[0] + '_interaction' + str(apply_interaction) \
-+ '_' + aggregation_method + '_' + padding_side + '.csv'
-if log_path in tuple(os.walk('logs/'))[0][2]:
-    print('exist', log_path)
-    log_path = 'logs/'+log_path
-else:
-    log_path = 'logs/'+log_path
-print(log_path)
-
-tokenizer = transformers.GPT2Tokenizer.from_pretrained(model_path, padding_side=padding_side)
-model = transformers.GPT2Model.from_pretrained(model_path)
-
+tokenizer = transformers.GPT2Tokenizer.from_pretrained('sberbank-ai/rugpt2large', padding_side=padding_side)
+tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+tokenizer.add_tokens({'end_of_masage_token': '[EOM]', 'end_of_masage_token': '[EOPS]'}, special_tokens=True)
+tokenizer.end_of_masage_token = '[EOM]'
+tokenizer.end_of_persona_sentence_token = '[EOPS]'
+model = transformers.GPT2Model.from_pretrained('sberbank-ai/rugpt2large')
+model.resize_token_embeddings(len(tokenizer))
+model.config.pad_token_id = tokenizer.pad_token_id
+model.to(device)
 models = [model]
-data = PersonaChatTorchDataset(proc_data)
+
+data = PersonaChatTorchDataset(config['proc_data_path'])
 split = len(data)//config['split']
 train, val = torch.utils.data.random_split(data, [len(data)-split, split])
 train = torch.utils.data.DataLoader(train, batch_size=32,
                         shuffle=True, num_workers=0, 
                         collate_fn=lambda x: clf(x, tokenizer_func=tokenize, 
-                                                 tokenizer=bert_tokenizer, 
+                                                 tokenizer=tokenizer, 
                                                  context_len=context_len, 
                                                  responce_len=responce_len, 
                                                  persona_len=persona_len))
 val = torch.utils.data.DataLoader(val, batch_size=32,
                         shuffle=True, num_workers=0, 
                         collate_fn=lambda x: clf(x, tokenizer_func=tokenize, 
-                                                 tokenizer=bert_tokenizer, 
+                                                 tokenizer=tokenizer, 
                                                  context_len=context_len, 
                                                  responce_len=responce_len, 
                                                  persona_len=persona_len))
 print(len(train), len(val))
 
-
 t_total = len(train) // gradient_accumulation_steps * 1
 
 for i, model in enumerate(models):
-    
     UNFREEZE_LAST_N = 8
     for param in list(model.parameters())[:-1]:
         param.requires_grad = False
-    for i, m in enumerate(model.transformer.h):        
+    for i, m in enumerate(model.h):        
         #Only un-freeze the last n transformer blocks
-        if i+1 > len(model.transformer.h) - UNFREEZE_LAST_N:
+        if i+1 > len(model.h) - UNFREEZE_LAST_N:
             print("un-freeze block number {} ".format(i+1))
             for parameter in m.parameters():
                 parameter.requires_grad = True 
 
-    for parameter in model.transformer.ln_f.parameters():        
+    for parameter in model.ln_f.parameters():        
         parameter.requires_grad = True
 
     optimizer = transformers.AdamW(filter(lambda p: p.requires_grad, model.parameters()),
@@ -96,7 +94,7 @@ for i, model in enumerate(models):
                                    )
 
     optimizers.append(optimizer)
-
+    
     if not test_mode:
         scheduler = transformers.get_linear_schedule_with_warmup(
             optimizer, num_warmup_steps=warmup_steps, num_training_steps=t_total
@@ -109,7 +107,6 @@ epoch_valid_accs = []
 epoch_valid_recalls = []
 epoch_valid_MRRs = []
 best_model_statedict = {}
-
 
 with open(log_path, 'w') as log:
     writer = csv.DictWriter(log, fieldnames=['epoch', 'train_loss', 'valid_loss', 'train_acc', 'valid_acc', 'valid_recall', 'valid_MRR'], delimiter=',', quotechar='"')
