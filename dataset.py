878d9e8ab51af6936749453c001463892fb7f63e
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2022-03-26 00:38:27 +0300

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -136,6 +136,10 @@ def clf(inp, tokenizer_func, tokenizer=False, context_len=32, responce_len=32, p
                 except KeyError:
                     pass
                 print('\n+++++++\n', line, '\n')
+                if persona_use == 'concat':
+                    for k in line['context']:
+                        line['context'][k] = torch.cat([line[k], line['persona'][k]])
+                print('\n+++++++\n', line, '\n')
                 if batch is None:
                     batch = line
                 else:
