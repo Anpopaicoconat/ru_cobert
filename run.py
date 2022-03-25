e29b4d4a2a8c5b386c83826f0982003945fee62f
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2022-03-26 01:16:38 +0300

    Update run.py

--- run.py
+++ run.py
@@ -130,7 +130,7 @@ train = torch.utils.data.DataLoader(train, batch_size=train_batch_size,
                                                  responce_len=responce_len, 
                                                  persona_len=persona_len,
                                                  type='bert',
-                                                 persona_use='split'))
+                                                 persona_use='concat'))
 val = torch.utils.data.DataLoader(val, batch_size=val_batch_size,
                         shuffle=True, num_workers=0, 
                         collate_fn=lambda x: clf(x, tokenizer_func=tokenize, 
@@ -139,7 +139,7 @@ val = torch.utils.data.DataLoader(val, batch_size=val_batch_size,
                                                  responce_len=responce_len, 
                                                  persona_len=persona_len,
                                                  type='bert',
-                                                 persona_use='concat'))
+                                                 persona_use='concat')) #concat
 print('\ntrain:', len(train), 'val:', len(val))
 t_total = len(train) // gradient_accumulation_steps * train_batch_size
 
