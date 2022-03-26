7b23e51774aad30e0793c5d9cc3a0736c0d76906
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2022-03-26 17:43:10 +0300

    Update run.py

--- run.py
+++ run.py
@@ -5,12 +5,16 @@ import transformers
 import numpy as np
 import tqdm
 import os
+from sklearn.model_selection import KFold
+
 from cobert import match, aggregate, fuse, dot_product_loss, train_epoch, evaluate_epoch
 from dataset import PersonaChatTorchDataset, clf, tokenize
 from util import logger
+
 def run(train, val, models, lr, t_total, epochs, has_persona, gradient_accumulation_steps, device, fp16, 
         amp, apply_interaction, matching_method, aggregation_method, epoch_train_losses, epoch_valid_losses, 
         epoch_valid_accs, epoch_valid_recalls, epoch_valid_MRRs, best_model_statedict, writer=None, save_model_path=False, test_mode=False):
+
     optimizers = []
     schedulers = []
     for i, model in enumerate(models):
@@ -33,6 +37,7 @@ def run(train, val, models, lr, t_total, epochs, has_persona, gradient_accumulat
         # training
         for model in models:
             model.train()
+
         train = tqdm.tqdm(train, desc="Iteration")
         train_loss, (train_acc, _, _) = train_epoch(data_iter=train, 
                                                     models=models, has_persona=has_persona, optimizers=optimizers, 
@@ -43,6 +48,7 @@ def run(train, val, models, lr, t_total, epochs, has_persona, gradient_accumulat
                                                     matching_method=matching_method, 
                                                     aggregation_method=aggregation_method)
         epoch_train_losses.append(train_loss)
+
         # evaluation
         for model in models:
             model.eval()
@@ -52,7 +58,8 @@ def run(train, val, models, lr, t_total, epochs, has_persona, gradient_accumulat
                                                                             gradient_accumulation_steps=gradient_accumulation_steps, 
                                                                             device=device, epoch=epoch, apply_interaction=apply_interaction, 
                                                                             matching_method=matching_method, aggregation_method=aggregation_method)
-        print("Epoch {0}: train loss: {1:.4f}, valid loss: {2:.4f}, train_acc: {3:.4f}, valid acc: {4:.4f}, valid recall: {5}, valid_MRR: {6:.4f}"
+
+        print("\nEpoch {0}: train loss: {1:.4f}, valid loss: {2:.4f}, train_acc: {3:.4f}, valid acc: {4:.4f}, valid recall: {5}, valid_MRR: {6:.4f}"
             .format(epoch+1, train_loss, valid_loss, train_acc, valid_acc, valid_recall, valid_MRR))
         if writer is not None:
             writer.writerow({'epoch':epoch+1, 'train_loss':train_loss, 'valid_loss': valid_loss, 'train_acc':train_acc, 
@@ -61,6 +68,7 @@ def run(train, val, models, lr, t_total, epochs, has_persona, gradient_accumulat
         epoch_valid_accs.append(valid_acc)
         epoch_valid_recalls.append(valid_recall)
         epoch_valid_MRRs.append(valid_MRR)
+
         if save_model_path:
             if epoch == 0:
                 for k, v in models.state_dict().items():
@@ -70,15 +78,20 @@ def run(train, val, models, lr, t_total, epochs, has_persona, gradient_accumulat
                     for k, v in models.state_dict().items():
                         best_model_statedict[k] = v.cpu()
                         
-with open('config.json', 'r') as config:
+with open('/content/drive/MyDrive/cobert/ru_cobert/config.json', 'r') as config:
     config  = json.loads(config.read())
+
 save_model_path = config['save_model_path']
+
 gradient_accumulation_steps = config['gradient_accumulation_steps']
+
 matching_method = config['matching_method']
+
 lr =  config['lr'] 
 warmup_steps = config['warmup_steps']
 test_mode = config['test_mode']
 has_persona = config['has_persona']
+
 context_len = config['context_len']
 responce_len = config['responce_len']
 persona_len = config['persona_len']
@@ -86,66 +99,91 @@ train_batch_size = config['train_batch_size']
 val_batch_size = config['val_batch_size']
 epochs = config['epochs']
 split = config['split']
+
 no_decay = ["bias", "LayerNorm.weight"]
 fp16 = False
 amp = None
 weight_decay = 0
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 bert_path = config['model_path']
 proc_data = config['proc_data_path']
 apply_interaction = config['apply_interaction']
 aggregation_method = config['aggregation_method']
 padding_side = config['padding_side']
+
+#k_folds = config['k_folds']
+k_folds = 5 #Возможно это можно записать в конфиг
+
 bert_config = transformers.BertConfig.from_pretrained(bert_path)
 bert_tokenizer = transformers.BertTokenizer.from_pretrained(bert_path, padding_side=padding_side)
-ctx_model = transformers.BertModel(bert_config).from_pretrained(bert_path)
-cnd_model = transformers.BertModel(bert_config).from_pretrained(bert_path)
-prs_model = transformers.BertModel(bert_config).from_pretrained(bert_path)
-all_models = [ctx_model, cnd_model, prs_model]
+
 data = PersonaChatTorchDataset(proc_data)
-split = len(data)//config['split']
-train, val = torch.utils.data.random_split(data, [len(data)-split, split])
-train = torch.utils.data.DataLoader(train, batch_size=train_batch_size,
-                        shuffle=True, num_workers=0, 
-                        collate_fn=lambda x: clf(x, tokenizer_func=tokenize, 
-                                                 tokenizer=bert_tokenizer, 
-                                                 context_len=context_len, 
-                                                 responce_len=responce_len, 
-                                                 persona_len=persona_len))
-val = torch.utils.data.DataLoader(val, batch_size=val_batch_size,
-                        shuffle=True, num_workers=0, 
-                        collate_fn=lambda x: clf(x, tokenizer_func=tokenize, 
-                                                 tokenizer=bert_tokenizer, 
-                                                 context_len=context_len, 
-                                                 responce_len=responce_len, 
-                                                 persona_len=persona_len))
-print('\ntrain:', len(train), 'val:', len(val))
-t_total = len(train) // gradient_accumulation_steps * train_batch_size
+
+# Define the K-fold Cross Validator
+kfold = KFold(n_splits=k_folds, shuffle=True)
+
 log_path = bert_path.split('/')[-1] + '_' + proc_data.split('/')[-1].split('.')[0] + '_interaction' + str(apply_interaction) +\
     '_' + aggregation_method + '_' + padding_side + '('+ str(lr) + ' ' + str(warmup_steps) + str(context_len)+ str(responce_len) +\
-    str(persona_len) + str(train_batch_size)+str(val_batch_size) + ')' + 'persona' + str(has_persona) + str(len(all_models)) + '.csv'
-log_path = 'logs/'+log_path
+    str(persona_len) + str(train_batch_size)+str(val_batch_size) + ')' + 'persona' + str(has_persona) + str(kfold) + '.csv'
+log_path = '/content/drive/MyDrive/cobert/results/'+log_path
 print(log_path)
-epoch_train_losses = []
-epoch_valid_losses = []
-epoch_valid_accs = []
-epoch_valid_recalls = []
-epoch_valid_MRRs = []
-best_model_statedict = {}
-                    
+
 with open(log_path, 'w') as log:
     writer = csv.DictWriter(log, fieldnames=['epoch', 'train_loss', 'valid_loss', 'train_acc', 'valid_acc', 'valid_r1', 'valid_r5', 'valid_r10', 'valid_MRR'],
                             delimiter=',', 
                             quotechar='"')
     writer.writeheader()
 
-    models = all_models[:1]
-    run(train, val, models, lr, t_total, 0, has_persona, gradient_accumulation_steps, device, fp16, 
-        amp, apply_interaction, matching_method, aggregation_method, epoch_train_losses, epoch_valid_losses, 
-        epoch_valid_accs, epoch_valid_recalls, epoch_valid_MRRs, best_model_statedict, writer, save_model_path, test_mode)
+    # K-fold Cross Validation model running
+    for fold, (train_ids, val_ids) in enumerate(kfold.split(data)):
+        ctx_model = transformers.BertModel(bert_config).from_pretrained(bert_path)
+        cnd_model = transformers.BertModel(bert_config).from_pretrained(bert_path)
+        prs_model = transformers.BertModel(bert_config).from_pretrained(bert_path)
+        all_models = [ctx_model, cnd_model, prs_model]
 
-    if has_persona:
-        [m.load_state_dict(models[0].state_dict()) for m in  all_models]
-        run(train, val, all_models, lr, t_total, epochs, has_persona, gradient_accumulation_steps, device, fp16, 
+        print(f"\nFold: {fold+1}")
+        # Sample elements randomly from a given list of ids, no replacement.
+        train_subsampler = torch.utils.data.SubsetRandomSampler(train_ids)
+        val_subsampler = torch.utils.data.SubsetRandomSampler(val_ids)
+
+        split = len(data)//config['split']
+        #train, val = torch.utils.data.random_split(data, [len(data)-split, split])
+
+        #For cross-validation shuffle=False, sampler=train_subsampler/val_subsampler
+        train = torch.utils.data.DataLoader(data, batch_size=train_batch_size,
+                                shuffle=False, num_workers=8, 
+                                sampler=train_subsampler,
+                                collate_fn=lambda x: clf(x, tokenizer_func=tokenize, 
+                                                        tokenizer=bert_tokenizer, 
+                                                        context_len=context_len, 
+                                                        responce_len=responce_len, 
+                                                        persona_len=persona_len))
+        val = torch.utils.data.DataLoader(data, batch_size=val_batch_size,
+                                shuffle=False, num_workers=8, 
+                                sampler=val_subsampler,
+                                collate_fn=lambda x: clf(x, tokenizer_func=tokenize, 
+                                                        tokenizer=bert_tokenizer, 
+                                                        context_len=context_len, 
+                                                        responce_len=responce_len, 
+                                                        persona_len=persona_len))
+        print('train:', len(train), 'val:', len(val))
+        t_total = len(train) // gradient_accumulation_steps * train_batch_size
+
+        epoch_train_losses = []
+        epoch_valid_losses = []
+        epoch_valid_accs = []
+        epoch_valid_recalls = []
+        epoch_valid_MRRs = []
+        best_model_statedict = {}
+
+        models = all_models[:1]
+        run(train, val, models, lr, t_total, 3, has_persona, gradient_accumulation_steps, device, fp16, 
             amp, apply_interaction, matching_method, aggregation_method, epoch_train_losses, epoch_valid_losses, 
             epoch_valid_accs, epoch_valid_recalls, epoch_valid_MRRs, best_model_statedict, writer, save_model_path, test_mode)
+        
+        if has_persona:
+            [m.load_state_dict(models[0].state_dict()) for m in  all_models]
+            run(train, val, all_models, lr, t_total, epochs, has_persona, gradient_accumulation_steps, device, fp16, 
+                amp, apply_interaction, matching_method, aggregation_method, epoch_train_losses, epoch_valid_losses, 
+                epoch_valid_accs, epoch_valid_recalls, epoch_valid_MRRs, best_model_statedict, writer, save_model_path, test_mode)
